056145ec72a59de0aacf0c2931d47efcf32fd8d1
maxmill03 <131804419+maxmill03@users.noreply.github.com>
2024-06-16 02:25:49 -0400

    Update db_airways.sql

--- db_airways.sql
+++ db_airways.sql
@@ -12,53 +12,91 @@ CREATE TABLE person (
 CREATE TABLE passenger (
   ticket_number VARCHAR(20),
   name VARCHAR(20),
-  PRIMARY KEY (ticket_number)
+  PRIMARY KEY (ticket_number),
   FOREIGN KEY name REFERENCES person(name)
 );
 
 CREATE TABLE employee (
-  employee_no INT(10)
-  name VARCHAR(20)
-  salary INT(20)
-  PRIMARY KEY (employee_no)
+  employee_no INT(10),
+  name VARCHAR(20),
+  salary INT(20),
+  PRIMARY KEY (employee_no),
   FOREIGN KEY name REFERENCES person(name)
 );
 
 CREATE TABLE departure (
-  date VARCHAR(20)
-  flight_number INT(10)
+  departure_date VARCHAR(20),
+  flight_number INT(10),
   PRIMARY KEY (dat, flight_number)
 );
 
 
 CREATE TABLE assigned_to (
-  employee_no INT(10)
-  date VARCHAR(20)
-  flight_number INT(10)
-  PRIMARY KEY (employee_no, date, flight_number)
-  FOREIGN KEY employee_no REFERENCES employee(employee_no)
+  employee_no INT(10),
+  date VARCHAR(20),
+  flight_number INT(10),
+  PRIMARY KEY (employee_no, date, flight_number),
+  FOREIGN KEY employee_no REFERENCES employee(employee_no),
   FOREIGN KEY (date, flight_number) REFERENCES departure(date, flight_number)
 );
 
 CREATE TABLE booked_on (
-  ticket_number VARCHAR(20)
-  date VARCHAR(20)
-  flight_number INT(10)
-  PRIMARY KEY (ticket_number, date, flight_number)
-  FOREIGN KEY (date, flight_number) REFERENCES departure(date, flight_number)
+  ticket_number VARCHAR(20),
+  date VARCHAR(20),
+  flight_number INT(10),
+  PRIMARY KEY (ticket_number, date, flight_number),
+  FOREIGN KEY (date, flight_number) REFERENCES departure(date, flight_number),
   FOREIGN KEY ticket_number REFERENCES passenger(ticket_number)
 );
 
 CREATE TABLE flight (
-  flight_number INT(10)
-  origin VARCHAR(5)
-  destination VARCHAR(5)
-  departure_time VARCHAR(5)
-  arrival_time VARCHAR(5)
-  status VARCHAR(10)
+  flight_number INT(10),
+  origin VARCHAR(5),
+  destination VARCHAR(5),
+  departure_time VARCHAR(5),
+  arrival_time VARCHAR(5),
+  status VARCHAR(10),
   PRIMARY KEY (flight_number)
 );
 
 CREATE TABLE instance_of (
-  
+  departure_date VARCHAR(20)
+  flight_number INT(10)
+  PRIMARY KEY (departure_date)
+  FOREIGN KEY flight_number REFERENCES flight(flight_number)
+
+);
+
+CREATE TABLE pilot (
+  flight_Hours INT(20),
+  employee_no INT(10),
+  PRIMARY KEY (employee_no),
+  FOREIGN KEY employee_no REFERENCES employee(employee_no)
+);
+
+CREATE TABLE plane (
+  maker VARCHAR(10),
+  model_no VARCHAR(10),
+  PRIMARY KEY (model_no),
+);
+
+CREATE TABLE aircraft (
+  serial_no INT(5),
+  model_no VARCHAR(10),
+  PRIMARY KEY (serial_no)
+);
+
+CREATE TABLE can_fly (
+  employee_no INT(10),
+  model_number VARCHAR(10),
+  PRIMARY KEY (employee_no, model_number),
+  FOREIGN KEY employee_no REFERENCES pilot(employee_no),
+  FOREIGN KEY model_number REFERENCES plane(model_no)
+);
+
+CREATE TABLE type (
+  serial_no INT(5)
+  model_no VARCHAR(10)
+  PRIMARY KEY (serial_no)
+  FOREIGN KEY model_no REFERENCES plane(model_no)
 );
